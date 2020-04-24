b6975c6d951ff7a9accf4011d8b07d0eba8221d6
Robert McBryde <robert.mcbryde@ros.gov.uk>
2020-04-24 11:32:18 +0100

    added functionality to add and delete new todo items plus some css styling of components

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,26 +1,33 @@
 import React, { useRef } from "react";
 
+import "./NewTodo.css";
+
 interface NewTodoProps {
-    onAddTodo: (todoText: string) => void;
+  onAddTodo: (todoText: string) => void;
 }
 
-
-const NewTodo: React.FC<NewTodoProps> = props => {
+const NewTodo: React.FC<NewTodoProps> = (props) => {
   const textInputRef = useRef<HTMLInputElement>(null);
 
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     if (textInputRef.current) {
-        const text = textInputRef.current.value;
-        props.onAddTodo(text);
+      const text = textInputRef.current.value;
+      const textInputElement = document.getElementById(
+        "todo-text"
+      ) as HTMLInputElement;
+      if (textInputElement) {
+        textInputElement.value = '';
+      }
+      props.onAddTodo(text);
     }
   };
 
   return (
     <form onSubmit={todoSubmitHandler}>
-      <div>
+      <div className="form-control">
         <label htmlFor="todo-text">Todo Text</label>
-        <input type="text" id="todo-text" ref={textInputRef}/>
+        <input type="text" id="todo-text" ref={textInputRef} />
       </div>
       <button type="submit">Add Todo</button>
     </form>
